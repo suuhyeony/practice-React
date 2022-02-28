cb069aee5c3916d1a255bca98deab6078893b3dc
suuhyeony <vv5835@naver.com>
2022-02-28 19:58:28 +0900

    organize & add new repositories

---
+++ practice-styling/src/components/CourseGoals/CourseInput/CourseInput.js
@@ -0,0 +1,66 @@
+import React, { useState } from 'react';
+// import styled from 'styled-components';
+import classes from './CourseInput.module.css';
+import Button from '../../UI/Button/Button';
+
+// const FormControl = styled.div`
+//   margin: 0.5rem 0;
+
+// & label {
+//   font-weight: bold;
+//   display: block;
+//   margin-bottom: 0.5rem;
+// }
+
+// & input {
+//   display: block;
+//   width: 100%;
+//   border: 1px solid ${props => (props.inValid ? 'red' : '#ccc')};
+//   font: inherit;
+//   line-height: 1.5rem;
+//   padding: 0 0.25rem;
+// }
+
+// & input:focus {
+//   outline: none;
+//   background: #fad0ec;
+//   border-color: #8b005d;
+// }
+
+// &.inValid input {
+//   border: 2px solid red;
+// }
+// `;
+
+const CourseInput = props => {
+  const [enteredValue, setEnteredValue] = useState('');
+  const [isValid, setIsValid] = useState(true);
+
+  const goalInputChangeHandler = event => {
+    if (event.target.value.trim().length > 0) {
+      setIsValid(true);
+    };
+    setEnteredValue(event.target.value);
+  };
+
+  const formSubmitHandler = event => {
+    event.preventDefault();
+    if (enteredValue.trim().length === 0) {
+      setIsValid(false);
+      return;
+    };
+    props.onAddGoal(enteredValue);
+  };
+
+  return (
+    <form onSubmit={formSubmitHandler}>
+      <div className={`${classes['form-control']} ${!isValid && classes.inValid}`}>
+        <label>Course Goal</label>
+        <input type="text" onChange={goalInputChangeHandler} />
+      </div>
+      <Button type="submit">Add Goal</Button>
+    </form>
+  );
+};
+
+export default CourseInput;
