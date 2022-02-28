f60ce6a16d491311b8f6a459c7a572e5f98a6a8c
suuhyeony <vv5835@naver.com>
2022-03-01 02:39:48 +0900

    use fragment & portals & refs

--- basic-form/src/components/Users/AddUser.js
+++ basic-form/src/components/Users/AddUser.js
@@ -1,60 +1,67 @@
 import Card from '../UI/Card';
 import Button from '../UI/Button';
 import classes from './AddUser.module.css';
-import { useState } from 'react';
+import { useRef, useState } from 'react';
 import ErrorModal from '../UI/ErrorModal';
 
 const AddUser = props => {
-  const [enteredUserName, setEnteredUserName] = useState('');
-  const [enteredAge, setEnteredAge] = useState('');
+  // const [enteredUserName, setEnteredUserName] = useState('');
+  // const [enteredAge, setEnteredAge] = useState('');
   const [error, setError] = useState();
+
+  // useState대신 useRef를 사용(=비제어 컴포넌트)하면, 코드는 줄지만 DOM을 건드린다는 단점이 있음 (읽어오거나 초기화 정도는 괜찮다)
+  const nameInputRef = useRef();
+  const ageInputRef = useRef();
 
   const addUserHandler = (event) => {
     event.preventDefault();
-    console.log(enteredUserName, enteredAge);
-    if (enteredUserName.trim().length === 0 || enteredAge.trim().length === 0) {
+    const nameValue = nameInputRef.current.value;
+    const ageValue = ageInputRef.current.value;
+
+    console.log(nameValue, ageValue);
+    if (nameValue.trim().length === 0 || ageValue.trim().length === 0) {
       setError({
         title: 'invalid input',
         message: 'please check your input (is there empty input?)'
       })
       return;
     }
-    if (+enteredAge < 1) {
+    if (+ageValue < 1) {
       setError({
         title: 'invalid age',
         message: 'please check your age (is smaller than 1?)'
       })
       return;
     }
-    props.onAddUser(enteredUserName, enteredAge);
-    setEnteredUserName('');
-    setEnteredAge('');
+    props.onAddUser(nameValue, ageValue);
+    nameInputRef.current.value = '';
+    ageInputRef.current.value = '';
   };
 
-  const userNameChangeHandler = (event) => {
-    setEnteredUserName(event.target.value);
-  };
-  const ageChangeHandler = (event) => {
-    setEnteredAge(event.target.value);
-  };
+  // const userNameChangeHandler = (event) => {
+  //   setEnteredUserName(event.target.value);
+  // };
+  // const ageChangeHandler = (event) => {
+  //   setEnteredAge(event.target.value);
+  // };
 
   const errorHandler = () => {
     setError(null);
   };
 
   return (
-    <div>
+    <>
       {error && <ErrorModal title={error.title} message={error.message} onConfirm={errorHandler} />}
       <Card className={classes.input}>
         <form onSubmit={addUserHandler}>
           <label htmlFor="name">userName</label>
-          <input id="name" type="text" value={enteredUserName} onChange={userNameChangeHandler} />
+          <input id="name" type="text" ref={nameInputRef} />
           <label htmlFor="age">age</label>
-          <input id="age" type="number" value={enteredAge} onChange={ageChangeHandler} />
+          <input id="age" type="number" ref={ageInputRef} />
           <Button type="submit">Add User</Button>
         </form>
       </Card>
-    </div>
+    </>
   );
 };
 
